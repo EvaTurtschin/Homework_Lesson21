7b161f75db862b63ec45ed48e36cc3e2cceb0d67
AIT TR Student <evgeniya@turtschin@gmail.com>
2023-12-07 15:05:51 +0100

    Homework_Task1_Lesson21

---
+++ src/homework_Dog/Dog.java
@@ -0,0 +1,22 @@
+package homework_Dog;
+
+public class Dog {
+
+    String dogName;
+    String dogBreed;
+    static int counter;
+
+    public Dog(String dogName, String dogBreed)
+    {
+        this.dogName = dogName;
+        this.dogBreed = dogBreed;
+        counter ++;
+    }
+
+
+  public String toString ()
+  { return dogName + " - " + dogBreed;
+  }
+
+
+}
---
+++ src/homework_Dog/Main.java
@@ -0,0 +1,22 @@
+package homework_Dog;
+
+public class Main {
+    public static void main(String[] args) {
+        /* Task 1
+        Создать класс Dog c полями name, counter и конструктором.
+        Cоздать несколько обьектов этого класса и вывести на печать количество созданных обьектов.
+         */
+
+        Dog d1 = new Dog("Bob", "Bobtail");
+        Dog d2 = new Dog("Frog", "Bulldog");
+        Dog d3 = new Dog("Mona", "German Shepherd");
+        Dog d4 = new Dog("Bucks", "Dachshund");
+
+        System.out.println("There are " + Dog.counter+ " dogs in our club.");
+
+        Dog [] dogs = {d1,d2,d3,d4};
+        for (Dog dog : dogs)
+        { System.out.println(dog);         }
+
+
+}}
