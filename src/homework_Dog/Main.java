1f7f469839013297e35bf28a37b20565cc3cd8c5
AIT TR Student <evgeniya@turtschin@gmail.com>
2023-12-07 15:44:27 +0100

    list counter added

--- src/homework_Dog/Main.java
+++ src/homework_Dog/Main.java
@@ -9,14 +9,18 @@ public class Main {
 
         Dog d1 = new Dog("Bob", "Bobtail");
         Dog d2 = new Dog("Frog", "Bulldog");
-        Dog d3 = new Dog("Mona", "German Shepherd");
-        Dog d4 = new Dog("Bucks", "Dachshund");
+        Dog d3 = new Dog("Mona", "Collie");
+        Dog d4 = new Dog("Bucks", "Bulldog");
 
         System.out.println("There are " + Dog.counter+ " dogs in our club.");
 
         Dog [] dogs = {d1,d2,d3,d4};
-        for (Dog dog : dogs)
-        { System.out.println(dog);         }
+       /* for (Dog dog : dogs)
+        { System.out.println(dog); }
+        */
+
+        for (int i=0, j=1; i< dogs.length; i++, j++)
+            System.out.println(j+ ". "+ dogs [i]);
 
 
 }}
